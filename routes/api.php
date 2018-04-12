99cf97b00f914bddb999d9050554a0f0e312ab81
Anne Jan Brouwer <brouwer@annejan.com>
2018-04-12 15:51:59 +0200

    ID

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,12 @@ use Illuminate\Http\Request;
 Route::get('sources', function () {
     return response()->json([
         [
+            'id' => 1,
             'name' => 'Nu.nl',
             'image' => 'https://bin.snmmd.nl/m/h25ykpju22mc_san_rectangle_xlarge.png/nu-nl-logo.png'
         ],
         [
+            'id' => 2,
             'name' => 'AD',
             'image' => 'https://simwave.nl/wp-content/uploads/2017/11/AD-logo-1.jpg'
         ]
