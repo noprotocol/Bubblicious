a74186d7dde07227d709b7b3c2f18f7794c0db73
Anne Jan Brouwer <brouwer@annejan.com>
2018-04-13 11:42:21 +0200

    User fixes

---
+++ database/migrations/2018_04_13_093733_alter_user_defaults.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUserDefaults extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('age')->unsigned()->default(18)->change();
+            $table->string('name')->nullable()->change();
+            $table->string('email')->nullable()->change();
+            $table->string('password')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
