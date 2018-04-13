27e2b266e862937505dc0a2b05433e61c595b029
Anne Jan Brouwer <brouwer@annejan.com>
2018-04-13 12:01:56 +0200

    Test added back

--- routes/api.php
+++ routes/api.php
@@ -81,10 +81,10 @@ Route::get('bubble', function (Request $request) {
 });
 
 Route::post('test', function (Request $request) {
-    dd('post', $request->header('X-Bubble'), $request);
+    return response()->json(['post', $request->header('X-Bubble'), $request]);
 });
 
 
 Route::get('test', function (Request $request) {
-    dd('get', $request->header('X-Bubble'), $request);
+    return response()->json(['get', $request->header('X-Bubble'), $request]);
 });
